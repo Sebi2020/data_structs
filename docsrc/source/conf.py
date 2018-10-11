9cd05fa5e6ff7260ab3b78b2ac9cb46d6c8a28d4
sebi2020 <v-apps@gmx.net>
2018-10-11 14:24:02 +0200

    corrected release in package.json and docs

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -24,9 +24,9 @@ copyright = u'2018, Sebastian Tilders'
 author = u'Sebastian Tilders'
 
 # The short X.Y version
-version = u'0.0'
+version = u'0.1'
 # The full version, including alpha/beta/rc tags
-release = u'0.0.1'
+release = u'0.1.1'
 
 
 # -- General configuration ---------------------------------------------------
